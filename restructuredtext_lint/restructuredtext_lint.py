0b0235f5ca909b12ff0fd00136e700b26eb41bf3
Todd Wolfson <todd@twolfson.com>
2013-11-22 01:28:14 -0800

    Iterated on restructured lint and have a good foundation

--- restructuredtext_lint/restructuredtext_lint.py
+++ restructuredtext_lint/restructuredtext_lint.py
@@ -2,14 +2,22 @@ import docutils
 from docutils.parsers.rst import Parser
 
 def run(content, filepath=None, **kwargs):
+    # Generate a new parser
     parser = Parser()
     settings = docutils.frontend.OptionParser(
                     components=(docutils.parsers.rst.Parser,)
                     ).get_default_values()
     document = docutils.utils.new_document(filepath, settings=settings)
-    def echo(data):
-        # print 'aaaaaaaaaaaa', data
-        pass
-    # document.reporter.error_handler = echo
-    print 'xxx', parser.parse(content, document)
-    return []
+
+    # Disable stdout
+    document.reporter.stream = None
+
+    # Collect errors via an observer
+    errors = []
+    def error_collector(data):
+        errors.append(data)
+    document.reporter.attach_observer(error_collector)
+
+    # Parse the content and return our collected errors
+    parser.parse(content, document)
+    return errors
