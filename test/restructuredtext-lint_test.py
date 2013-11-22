2388384a57447af1a346fa1aa02cf5860e43f5a2
Todd Wolfson <todd@twolfson.com>
2013-11-22 01:55:56 -0800

    Updating assertions and TODOs

--- test/restructuredtext-lint_test.py
+++ test/restructuredtext-lint_test.py
@@ -36,7 +36,7 @@ class TestRestructuredtextLint(TestCase):
         """Lint the file and preserve any errors"""
         return restructuredtext_lint.run(*args)
 
-    # TODO: Consider implementing these as flat file tests once we know what the output looks like
+    # TODO: Move to flat file tests
     def test_passes_valid_rst(self):
         """A valid reStructuredText file will not raise any errors"""
         filepath = __dir__ + '/test_files/valid.rst'
@@ -52,8 +52,4 @@ class TestRestructuredtextLint(TestCase):
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0].line, 2)
         self.assertEqual(errors[0].source, filepath)
-        self.assertEqual(errors[0].message, """Title underline too short.
-
-Hello
-====""")
-        # TODO: Assert actual errors
+        self.assertEqual(errors[0].message, 'Title underline too short.')
