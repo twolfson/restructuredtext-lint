bed9a06d3600c02f4cab8dd1e8dd55441bc670c3
Todd Wolfson <todd@twolfson.com>
2014-02-26 21:36:48 -0800

    Release 0.3.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,5 +1,7 @@
 restructuredtext-lint changelog
 ===============================
+0.3.0 - Moved from `read` to `io.read`, allowing specification of `encoding` from CLI. Attribution to @coldfix
+
 0.2.0 - Added CLI utility
 
 0.1.1 - Repairing link for PyPI
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='restructuredtext_lint',
-    version='0.2.0',
+    version='0.3.0',
     description='reStructuredText linter',
     long_description=open('README.rst').read(),
     keywords=[
