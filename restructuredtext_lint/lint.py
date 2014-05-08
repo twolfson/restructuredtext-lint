d90024761b6dfb576031e9db46c09f1750f50005
Todd Wolfson <todd@twolfson.com>
2014-05-08 00:04:56 -0700

    Attempting to see if we overkilled transformers

--- restructuredtext_lint/lint.py
+++ restructuredtext_lint/lint.py
@@ -55,17 +55,7 @@ def lint(content, filepath=None):
     document.reporter.attach_observer(error_collector)
 
     # Parse the content and return our collected errors
-    transformer = document.transformer
-    while transformer.transforms:
-        if not transformer.sorted:
-            # Unsorted initially, and whenever a transform is added.
-            transformer.transforms.sort()
-            transformer.transforms.reverse()
-            transformer.sorted = 1
-        priority, transform_class, pending, kwargs = transformer.transforms.pop()
-        transform = transform_class(transformer.document, startnode=pending)
-        transform.apply(**kwargs)
-        transformer.applied.append((priority, transform_class, pending, kwargs))
+    document.transformer.apply_transforms()
     print 'wat', errors
     return errors
 
