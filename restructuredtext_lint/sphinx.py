be210ddf999abeb6135ae070abbdbdebcf5770a9
Joshua Harlow <harlowja@yahoo-inc.com>
2015-04-09 12:35:13 -0700

    Remove extra period (again)

--- restructuredtext_lint/sphinx.py
+++ restructuredtext_lint/sphinx.py
@@ -138,7 +138,7 @@ def unregister_ignores(registered_directives, registered_roles):
 
 @contextlib.contextmanager
 def register_unregister_ignores(no_skip_directives=None, no_skip_roles=None):
-    """Register then unregister ignoreable sphinx directives & roles..
+    """Register then unregister ignoreable sphinx directives & roles.
 
     Does nothing if sphinx is not importable.
 
