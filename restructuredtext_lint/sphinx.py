a958dd998f6e431ec2d518297f94a54eab2fa166
Todd Wolfson <todd@twolfson.com>
2015-04-10 01:34:52 -0500

    Fixing up more links

--- restructuredtext_lint/sphinx.py
+++ restructuredtext_lint/sphinx.py
@@ -7,7 +7,7 @@ from .utils import EmptyDirective, get_empty_role
 BUILTIN_DOMAINS = None
 try:
     # Import all known sphinx domains
-    # See: https://github.com/sphinx-doc/sphinx/tree/1.3/sphinx/domains
+    #   https://github.com/sphinx-doc/sphinx/tree/1.3/sphinx/domains
     from sphinx.domains import BUILTIN_DOMAINS
 except ImportError:
     pass
@@ -15,9 +15,9 @@ except ImportError:
 
 # Define our constants
 #   Default/base roles and directives for Sphinx
-# See: http://sphinx-doc.org/markup/para.html
-# And: http://sphinx-doc.org/markup/toctree.html#directive-toctree
-# And: https://github.com/sphinx-doc/sphinx/blob/1.3/sphinx/directives/other.py
+#   http://sphinx-doc.org/markup/para.html
+#   http://sphinx-doc.org/markup/toctree.html#directive-toctree
+#   https://github.com/sphinx-doc/sphinx/blob/1.3/sphinx/directives/other.py
 
 # TODO: Is this really neecessary? We should be able to leverage BUILTIN_DOMAINS
 
