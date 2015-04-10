866e978f5851637cf6fb33767eae7315ebbf4826
Todd Wolfson <todd@twolfson.com>
2015-04-10 00:40:50 -0500

    Adjusting to fullernames

--- restructuredtext_lint/sphinx.py
+++ restructuredtext_lint/sphinx.py
@@ -1,14 +1,10 @@
 from __future__ import absolute_import
 
+SPHINX_AVAILABLE = True
 try:
-    import sphinx  # noqa
-
-    # All currently know sphinx domains.
-    #
+    # Import all known sphinx domains
     # See: https://github.com/sphinx-doc/sphinx/tree/1.3/sphinx/domains
     from sphinx import domains
-
-    SPHINX_AVAILABLE = True
 except ImportError:
     SPHINX_AVAILABLE = False
 
@@ -17,11 +13,11 @@ except ImportError:
 # See: http://sphinx-doc.org/markup/para.html
 # And: http://sphinx-doc.org/markup/toctree.html#directive-toctree
 # And: https://github.com/sphinx-doc/sphinx/blob/1.3/sphinx/directives/other.py
-_base_sp_roles = ('ctype',)
-_base_sp_directives = ('autosummary', 'centered', 'currentmodule',
-                       'deprecated', 'hlist', 'include', 'index',
-                       'literalinclude', 'no-code-block', 'seealso',
-                       'toctree', 'todo', 'versionadded', 'versionchanged')
+_base_sphinx_roles = ('ctype',)
+_base_sphinx_directives = ('autosummary', 'centered', 'currentmodule',
+                           'deprecated', 'hlist', 'include', 'index',
+                           'literalinclude', 'no-code-block', 'seealso',
+                           'toctree', 'todo', 'versionadded', 'versionchanged')
 
 
 def fetch_roles_directives():
@@ -37,8 +33,8 @@ def fetch_roles_directives():
                            ' to. Please verify `sphinx` is installed'
                            ' properly.')
 
-    sp_directives = list(_base_sp_directives)
-    sp_roles = list(_base_sp_roles)
+    sphinx_directives = list(_base_sphinx_directives)
+    sphinx_roles = list(_base_sphinx_roles)
 
     # Get all the domains directives and roles and insert them.
     for name, domain_class in domains.BUILTIN_DOMAINS.items():
@@ -49,19 +45,19 @@ def fetch_roles_directives():
         # Ensure that we also use the name prefixed version as well
         # for example :py:func: and :func: are equivalent and we need to make
         # sure we register both kinds.
-        sp_directives.extend(domain_directives)
+        sphinx_directives.extend(domain_directives)
         if name != 'std':
-            sp_directives.extend('{domain}:{item}'.format(domain=domain_class.name,
-                                                          item=item)
+            sphinx_directives.extend('{domain}:{item}'.format(domain=domain_class.name,
+                                                              item=item)
                                  for item in domain_directives)
 
-        sp_roles.extend(domain_roles)
+        sphinx_roles.extend(domain_roles)
         if name != 'std':
-            sp_roles.extend('{domain}:{item}'.format(domain=domain_class.name,
-                                                     item=item)
+            sphinx_roles.extend('{domain}:{item}'.format(domain=domain_class.name,
+                                                         item=item)
                             for item in domain_roles)
 
     return {
-        'directives': sp_directives,
-        'roles': sp_roles,
+        'directives': sphinx_directives,
+        'roles': sphinx_roles,
     }
