1d0f0df3be48276e443f5fef4afeeea7bd82ff74
Todd Wolfson <todd@twolfson.com>
2015-04-10 00:52:42 -0500

    Updated test to fail on bad lint

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+# Exit on first error and echo commands
+set -e
+set -x
+
+# Run our linter and tests
 flake8 --max-line-length=120 restructuredtext_lint
-nosetests --nocapture
+nosetests --nocapture $*
