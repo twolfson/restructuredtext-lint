780bcf37a963b06ef7ea6be56ecd68b72dd1790b
Todd Wolfson <todd@twolfson.com>
2015-04-10 22:29:57 -0500

    Looks like domain specific directives/roles aren't as easy to add

--- restructuredtext_lint/test/test.py
+++ restructuredtext_lint/test/test.py
@@ -4,7 +4,6 @@ from unittest import TestCase
 import yaml
 
 import restructuredtext_lint
-from restructuredtext_lint.sphinx import get_empty_directives_roles
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
@@ -105,5 +104,6 @@ class TestRestructuredtextLint(TestCase):
         https://github.com/twolfson/restructuredtext-lint/issues/11
         """
         filepath = __dir__ + '/test_files/sphinx.rst'
-        errors = restructuredtext_lint.lint_file(filepath, **get_empty_directives_roles())
+        from sphinx.python import PythonDomain
+        errors = restructuredtext_lint.lint_file(filepath)
         self.assertEqual(errors, [])
