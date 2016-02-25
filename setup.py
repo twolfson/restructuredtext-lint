fe60817d67fdef672231c30385d99afd50614f39
Todd Wolfson <todd@twolfson.com>
2016-02-25 03:12:10 -0600

    Release 0.14.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='restructuredtext_lint',
-    version='0.14.0',
+    version='0.14.1',
     description='reStructuredText linter',
     long_description=open('README.rst').read(),
     keywords=[
