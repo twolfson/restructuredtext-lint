bd7e2eafd3b519c93d4b5ed1f9fa442272002c89
Todd Wolfson <todd@twolfson.com>
2020-06-02 17:42:57 -0700

    Added .pyc scrubbing. Fixes #56

--- release.sh
+++ release.sh
@@ -5,6 +5,11 @@ set -e
 # Install our dependencies
 npm install foundry@~4.3.2 foundry-release-git@~2.0.2 foundry-release-pypi@~3.0.0
 
+# Remove all `.pyc` files
+shopt -s globstar
+rm **/*.pyc
+shopt -u globstar
+
 # Run foundry release with an adjusted PATH
 PATH="$PATH:$PWD/node_modules/.bin/"
 foundry release $*
